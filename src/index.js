09d8ec5ce8a235b73fe50bd9cc6c060f7e6a34c8
Christian W. <wood.c9595@gmail.com>
2024-08-11 14:50:58 -0500

    Adding, Removing, and Viewing people from array now possible

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const {Client, IntentsBitField} = require('discord.js');
+const {Client, IntentsBitField, Collection} = require('discord.js');
 
 const client = new Client({
     intents: [
@@ -11,13 +11,75 @@ const client = new Client({
     ],
 });
 
+const userStates = new Collection();
+const dinSchArry = ['NameA', 'NameB', 'NameC'];
+
 client.on('ready', (c) => {
     console.log('the bot is ready');
 });
 
-client.on('messageCreate', (message) => {
-    if (message.content == '!DinDin'){
-        message.reply('This is where the schedule will pop up!');
+client.on('messageCreate', async (message) =>{
+    if (message.author.bot){
+        return;
+    ;}
+
+    const userId = message.author.id;
+    const addWaiting = userStates.get(userId);
+
+    //Addding People to Schedule
+    if (addWaiting === 'waiting_to_add'){
+        const addResponse = message.content;
+        console.log(message.content);
+
+        await message.channel.send(`You will be adding: "${addResponse}" to the DinDin Schdule.`);
+        dinSchArry.push(addResponse);
+
+        console.log(dinSchArry);
+
+        userStates.delete(userId);
+        return;
+    }
+
+    if (message.content == '!addToSch'){
+        await message.channel.send('Who would you like to Add? Please only type the name.');
+
+        userStates.set(userId, 'waiting_to_add');
+    };
+
+    //Removing People from Schedule
+    if (addWaiting === 'waiting_to_remove'){
+        const removeResponse = message.content;
+        console.log(message.content);
+
+        if(dinSchArry.includes(removeResponse)){
+            await message.channel.send(`You have removed... "${removeResponse}"`);
+
+            var remIndex = dinSchArry.indexOf(removeResponse);
+            
+            dinSchArry.splice(remIndex, 1);
+        } else {
+            await message.channel.send(`${removeResponse} is not in the schedule.`);
+        }
+        //dinSchArry.push(reResponse);
+
+        console.log(dinSchArry);
+
+        userStates.delete(userId);
+        return;
+    }
+
+    if (message.content == '!remFromSch'){
+        const scheduleResp = dinSchArry.join('\n');
+        await message.channel.send(`Who would you like to remove?\n${scheduleResp}`);
+
+        userStates.set(userId, 'waiting_to_remove');
+    };
+
+    //Checking Schedule
+    if (message.content == '!din'){
+        const scheduleResp = dinSchArry.join('\n');
+
+        message.channel.send(scheduleResp);
     }
 });
 
