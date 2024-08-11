c61819df3522032232389f0b34dcee8b95184f9b
Christian W. <wood.c9595@gmail.com>
2024-08-11 16:10:51 -0500

    Switching people now added

--- src/index.js
+++ src/index.js
@@ -26,30 +26,45 @@ client.on('messageCreate', async (message) =>{
     const userId = message.author.id;
     const addWaiting = userStates.get(userId);
 
-    //Addding People to Schedule
-    if (addWaiting === 'waiting_to_add'){
-        const addResponse = message.content;
-        console.log(message.content);
-
-        await message.channel.send(`You will be adding: "${addResponse}" to the DinDin Schdule.`);
-        dinSchArry.push(addResponse);
-
-        console.log(dinSchArry);
-
-        userStates.delete(userId);
-        return;
+    //List of Commands
+    if (message.content == "!botCommands"){
+        message.channel.send("View Din Din Schedule: **!din**\nAdd To Schedule: **!addToSch**\nRemove From Schedule: **!remFromSch**\n");
     }
 
+     //Checking Schedule
+     if (message.content == '!din'){
+        const scheduleResp = dinSchArry.join('\n');
+
+        message.channel.send(scheduleResp);
+    }
+
+    //Addding People to Schedule
     if (message.content == '!addToSch'){
         await message.channel.send('Who would you like to Add? Please only type the name.');
 
         userStates.set(userId, 'waiting_to_add');
     };
 
+    if (addWaiting === 'waiting_to_add'){
+        const addResponse = message.content;
+
+        await message.channel.send(`You will be adding: "${addResponse}" to the DinDin Schdule.`);
+        dinSchArry.push(addResponse);
+
+        userStates.delete(userId);
+        return;
+    }
+
     //Removing People from Schedule
+    if (message.content == '!remFromSch'){
+        const scheduleResp = dinSchArry.join('\n');
+        await message.channel.send(`Who would you like to remove?\n${scheduleResp}`);
+
+        userStates.set(userId, 'waiting_to_remove');
+    };
+
     if (addWaiting === 'waiting_to_remove'){
         const removeResponse = message.content;
-        console.log(message.content);
 
         if(dinSchArry.includes(removeResponse)){
             await message.channel.send(`You have removed... "${removeResponse}"`);
@@ -60,27 +75,55 @@ client.on('messageCreate', async (message) =>{
         } else {
             await message.channel.send(`${removeResponse} is not in the schedule.`);
         }
-        //dinSchArry.push(reResponse);
-
-        console.log(dinSchArry);
 
         userStates.delete(userId);
         return;
     }
 
-    if (message.content == '!remFromSch'){
+    //Switching Positions in Schedule
+    if (message.content == '!switchDuties'){
         const scheduleResp = dinSchArry.join('\n');
-        await message.channel.send(`Who would you like to remove?\n${scheduleResp}`);
+        await message.channel.send(`Which two people would you like to switch?\n${scheduleResp}`);
 
-        userStates.set(userId, 'waiting_to_remove');
-    };
-
-    //Checking Schedule
-    if (message.content == '!din'){
-        const scheduleResp = dinSchArry.join('\n');
-
-        message.channel.send(scheduleResp);
+        userStates.set(userId, 'waiting_to_switch_part1');
     }
+
+    if (addWaiting === 'waiting_to_switch_part1'){
+        switchRespA = message.content;
+
+        if(dinSchArry.includes(switchRespA)){
+            tempDinSch = dinSchArry.slice();
+            var remIndex = tempDinSch.indexOf(switchRespA);
+            tempDinSch.splice(remIndex, 1);
+
+            const tempScheduleResp = tempDinSch.join('\n');
+
+            await message.channel.send(`Who is switching with ${switchRespA}?\n${tempScheduleResp}`);
+            userStates.set(userId, 'waiting_to_switch_part2');
+        } else {
+            await message.channel.send(`${switchRespA} is not someone on the schedule. Try again.`);
+        }
+
+    } else if (addWaiting === 'waiting_to_switch_part2'){
+        const switchRespB = message.content;
+
+        if(tempDinSch.includes(switchRespB)){
+            var switchIndexA = dinSchArry.indexOf(switchRespA);
+            var switchIndexB = dinSchArry.indexOf(switchRespB);
+
+            [dinSchArry[switchIndexA], dinSchArry[switchIndexB]] = [dinSchArry[switchIndexB], dinSchArry[switchIndexA]];
+
+            const scheduleResp = dinSchArry.join('\n');
+
+            await message.channel.send(`${switchRespA} will be swapped with ${switchRespB}!\n${scheduleResp}`);
+
+            userStates.delete(userId);
+            return;
+        } else {
+            await message.channel.send(`${switchRespB} is not someone on the schedule. Try again.`);
+        }
+        
+     }
 });
 
 client.login(process.env.TOKEN);
